3f0d7ef9b4d9f15c1d78cdd34e106f13c165633d
risa <cherry881113@gmail.com>
2024-07-07 02:06:53 +0800

    AR Function

---
+++ 2024_APP_Competition/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  2024_APP_Competition
+//
+//  Created by 彭惠暄 on 2024/7/7.
+//
+
+import Foundation
---
+++ 2024_APP_Competition/ViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController.swift
+//  2024_APP_Competition
+//
+//  Created by 彭惠暄 on 2024/7/7.
+//
+
+import Foundation
