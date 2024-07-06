308af3b3f578c562c27d35fecd10b1a337d7362f
risa <cherry881113@gmail.com>
2024-07-07 02:01:50 +0800

    Initial Commit

---
+++ 2024_APP_Competition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2024_APP_Competition
+//
+//  Created by 彭惠暄 on 2024/7/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024_APP_Competition/_024_APP_CompetitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024_APP_CompetitionApp.swift
+//  2024_APP_Competition
+//
+//  Created by 彭惠暄 on 2024/7/7.
+//
+
+import SwiftUI
+
+@main
+struct _024_APP_CompetitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
